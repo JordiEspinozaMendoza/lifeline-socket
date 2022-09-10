b818334cbaeba26200d54d0a517c8a56dca44b1c
Jordi <jordi8101@gmail.com>
2022-09-09 21:44:59 -0700

    init proyect

---
+++ config/config.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+module.exports = {
+  PORT: process.env.PORT,
+  REDIS: {
+    HOST: process.env.REDIS_HOST,
+    PORT: process.env.REDIS_PORT,
+    CADUCIDAD_DEFAULT: 3600,
+  },
+};
---
+++ server/index.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+/* AUTHENTICATION SERVER NODE */
+const app = express();
+const server = http.createServer(app);
+export default server;
